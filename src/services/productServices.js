97e5bbf50f5ab5b1d3e2a0972527258b644ae91f
Brendon <bluicien@yahoo.com>
2025-06-03 23:34:14 +1200

    Added basic DB functions

--- src/services/productServices.js
+++ src/services/productServices.js
@@ -1,5 +1,36 @@
 import { Product } from "../models/product";
 
-export const getAllProducts = () => {
-    const allProducts = Product.find({});
+export const getAllProducts = async () => {
+    const allProducts = await Product.find({});
+    return allProducts;
+}
+
+export const getProductByTitle = async (title) => {
+    if (!title) return null; // If no product is entered. Return nothing.
+
+    const product = await Product.find({title});
+    return product;
+}
+
+export const addProduct = async (title, description = "", startPrice, reservePrice) => {
+    if (!title && parseFloat(startPrice === "NaN" && parseFloat(reservePrice) === "NaN")) {
+        console.error("Missing required data fields, or invalid data types.");
+        return;
+    }
+
+    const newProduct = new Product({
+        title: title, description,
+        description: description, 
+        startPrice: Number(startPrice),
+        reservePrice: Number(reservePrice)
+    });
+    newProduct.save();
+
+}
+
+export const deleteProductById = async (_id) => {
+    if (!_id) return;
+
+    await Product.deleteOne({_id});
+    return;
 }
